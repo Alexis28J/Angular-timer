9461aea5294c49e46a6b8848676fb7e36423ac20
Alexis28J <jordy2806@hotmail.com>
2026-02-03 21:05:05 +0100

    Primo passo

--- src/app/services/clock-service.ts
+++ src/app/services/clock-service.ts
@@ -1,16 +1,39 @@
-import { inject, Injectable, signal, WritableSignal } from '@angular/core';
+import { computed, inject, Injectable, signal, WritableSignal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClockService {
-time: WritableSignal<Date> = signal(new Date());
 
-constructor(){
-  setInterval(() => {
-     
-    this.time.set(new Date());
-    
-  }, 1000);
-}
+  time: WritableSignal<Date> = signal(new Date());
+
+
+  secondsToMidnight = computed(() => {
+
+    const todayAtMidnight = new Date();
+    todayAtMidnight.setHours(24, 0, 0, 0);
+    const todayAtMidnightInSeconds = Math.round(todayAtMidnight.getTime() / 1000);
+
+    const timeInSeconds = Math.round(this.time().getTime() / 1000);
+
+    const secondsleft = todayAtMidnightInSeconds - timeInSeconds;
+
+    return secondsleft;
+  })
+
+  constructor() {
+    setInterval(() => {
+
+      this.time.set(new Date());
+
+      //metodo senza usare set - è più lungo e macchinoso - non consigliato
+      // this.time.update(oldTime => {
+      //   console.log(oldTime);
+      //   oldTime.setSeconds(oldTime.getSeconds() + 1);
+      //   console.log(oldTime);
+      //   return new Date(oldTime);
+      // });
+
+    }, 1000);
+  }
 }
