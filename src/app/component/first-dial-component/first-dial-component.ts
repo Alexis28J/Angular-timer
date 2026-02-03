87c12f0ae2fb71306835184a75860cf376c5e66e
Alexis28J <jordy2806@hotmail.com>
2026-02-03 22:51:59 +0100

    Primo quadrante 2

--- src/app/component/first-dial-component/first-dial-component.ts
+++ src/app/component/first-dial-component/first-dial-component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ClockService } from '../../services/clock-service';
+
 
 @Component({
   selector: 'app-first-dial-component',
@@ -7,7 +8,10 @@ import { ClockService } from '../../services/clock-service';
   templateUrl: './first-dial-component.html',
   styleUrl: './first-dial-component.scss',
 })
+
+
 export class FirstDialComponent {
+
 
 timeInSeconds() {
 console.log('Secondi');
@@ -17,10 +21,15 @@ return this.currentTime.secondsToMidnight();
 
 timeInMinutes() {
 console.log('Minuti');
-
+//console.log(this.currentTime.minutesToMidnight());
+return this.currentTime.minutesToMidnight();
+;
 }
+
 timeInHours() {
 console.log('Ore');
+console.log(this.currentTime.hoursToMidnigth());
+return this.currentTime.hoursToMidnigth();
 }
 
 currentTime = inject(ClockService);
--- src/app/services/clock-service.ts
+++ src/app/services/clock-service.ts
@@ -12,9 +12,10 @@ export class ClockService {
 
   secondsToMidnight = computed(() => {
 
-    const todayAtMidnight = new Date();
+    const todayAtMidnight = new Date();  //new Date() in JavaScript viene utilizzato per creare un nuovo oggetto che memorizza data e ora correnti, o una specifica data e ora.
     todayAtMidnight.setHours(24, 0, 0, 0);
     const todayAtMidnightInSeconds = Math.round(todayAtMidnight.getTime() / 1000);
+    //Il metodo getTime() in JavaScript serve a ottenere il valore numerico della data specificata, rappresentato dal numero di millisecondi trascorsi dall'epoca Unix (1 gennaio 1970, 00:00:00 UTC).
 
     const timeInSeconds = Math.round(this.time().getTime() / 1000);
 
@@ -23,34 +24,58 @@ export class ClockService {
     return secondsleft;
   })
 
+  minutesToMidnight = computed(() => {
+    const todayAtMidnight = new Date();
+    todayAtMidnight.setHours(24, 0, 0, 0);
+    const todayAtMidnightInMinutes = Math.round(todayAtMidnight.getTime() / 60000);
+
+    const timeInMinutes = Math.round(this.time().getTime() / 60000);
+
+    const minutesLeft = todayAtMidnightInMinutes - timeInMinutes;
+
+    return minutesLeft;
+  })
+
+  hoursToMidnigth = computed(() => {
+    const todayAtMidnight = new Date();
+    todayAtMidnight.setHours(24, 0, 0, 0);
+    const todayAtMidnightInHours = Math.round(todayAtMidnight.getTime() / 3600000);
+
+    const timeInHours = Math.round(this.time().getTime() / 3600000)
+
+    const hoursLeft = todayAtMidnightInHours - timeInHours;
+
+    return hoursLeft;
+
+  })
 
 
-//   formattedTime = computed(() =>
-//   this.time().toLocaleTimeString('it-IT', {
-//     hour: '2-digit',
-//     minute: '2-digit',
-//     second: '2-digit'
-//   })
-// );
+  //   formattedTime = computed(() =>
+  //   this.time().toLocaleTimeString('it-IT', {
+  //     hour: '2-digit',
+  //     minute: '2-digit',
+  //     second: '2-digit'
+  //   })
+  // );
 
-formattedTime = computed(() => {
-  const clock = this.time();
-  return clock.toLocaleDateString('it-IT', {
-    // weekday: 'long',
-    day: '2-digit',
-    month: 'short',   //long = febbraio, short = feb
-    year: 'numeric'
-  }) + '  -  ' + clock.toLocaleTimeString('it-IT', {
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit'
+  formattedTime = computed(() => {
+    const clock = this.time();
+    return clock.toLocaleDateString('it-IT', {
+      // weekday: 'long',
+      day: '2-digit',
+      month: 'short',   //long = febbraio, short = feb
+      year: 'numeric'
+    }) + '  -  ' + clock.toLocaleTimeString('it-IT', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    });
   });
-});
 
   constructor() {
-    setInterval(() => {    
+    setInterval(() => {
 
-      this.time.set(new Date()); 
+      this.time.set(new Date());
 
       //metodo senza usare set - è più lungo e macchinoso - non consigliato
       // this.time.update(oldTime => {
@@ -61,6 +86,6 @@ formattedTime = computed(() => {
       // });
 
     }, 1000);
-    
+
   }
 }
